e14a8130f30ecdfd285e3d3ffc74efc518702ae6
Jan Palas <jan.palas@gmail.com>
2017-06-02 14:41:52 +0200

    Testing EDMX

---
+++ Source/EfFirebird.Model.Tests/Tests/EdmxTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+
+namespace EfFirebird.Model.Tests.Tests
+{
+    [TestFixture]
+    public class EdmxTests
+    {
+        //private PudaDbContext _dbContext;
+
+        //[SetUp]
+        //public void SetUp()
+        //{
+        //    _dbContext = new PudaDbContext();
+        //}
+
+        //[TearDown]
+        //public void CleanUp()
+        //{
+        //    _dbContext.Dispose();
+        //}
+
+        //[Test]
+        //public void SimpleRead()
+        //{
+        //    //arrange
+
+
+        //    //act
+
+        //    List<AKCE> akce = _dbContext.Set<AKCE>().ToList();
+
+        //    //assert
+        //    Assert.IsNotNull(akce);
+        //    Assert.IsNotEmpty(akce);
+        //}
+    }
+}
